67dcf4abee2f5891e2a3d860e887334f11ffa543
RogerC17 <rcalvoa10@gmail.com>
2023-05-07 21:56:25 -0500

    RegisterForm

--- src/App.js
+++ src/App.js
@@ -4,12 +4,13 @@ import LoginForm from './LoginForm';
 import HomePage from './HomePage';
 import AsNivel from './AsNivel';
 import AsMetodo from './AsMetodo';
+import RegisterPage from './RegisterForm';
 
 function App() {
   return (
     <div className="gradient-custom">
       <h1 className="fw-bold text-uppercase text-white mb-2">Bienvenido estudioso</h1>
-      <AsMetodo />
+      <RegisterPage />
     </div>
   );
 }
